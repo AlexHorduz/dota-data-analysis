d2a39744b8ee5017ed9ce93274cf2f2c1f9845ed
AlexHorduz <alex.horduz@gmail.com>
2024-05-26 13:09:49 +0300

    Add minimum time to update

--- data_updater/api/endpoints.py
+++ data_updater/api/endpoints.py
@@ -1,10 +1,10 @@
+from time import time
 
 from fastapi import APIRouter
 
 from etl.update_service import UpdateService
 from etl.synthetic_data_parser import SyntheticDataParser
 from etl.open_dota_api_parser import OpenDotaParser
-from .models import UserHeroModel
 from core.config import settings
 
 router = APIRouter()
@@ -15,6 +15,16 @@ else:
     pars = OpenDotaParser()
 upd = UpdateService(pars)
 
+
+LAST_UPDATE_TIME = None
+
 @router.post("/updateData")
 async def update_data():
+    global LAST_UPDATE_TIME
+
+    if LAST_UPDATE_TIME and ((time() - LAST_UPDATE_TIME) < settings.MIN_TIME_TO_UPDATE):
+        return {"message": f"Please wait {int(settings.MIN_TIME_TO_UPDATE - (time() - LAST_UPDATE_TIME))} seconds until the next update"}
+    
     upd.update_all_data()
+    LAST_UPDATE_TIME = time()
+    return {"message": "Data has been updated successfully"}
--- data_updater/core/config.py
+++ data_updater/core/config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     SQL_DATABASE_URL: str
     DEBUG: bool
+    MIN_TIME_TO_UPDATE: int
 
     class Config:
         env_file = ".env"
